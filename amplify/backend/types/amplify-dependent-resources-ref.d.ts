c18ad27e976964cb94fde74830250343d9ef53f0
Tetsuhiro Nagasaki <nagasaki.tetsuhiro@xchange.jp>
2023-06-15 17:41:51 +0900

    認証

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "viteproject20e6b5dc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <main>
-    <header>
-      <h2>Amplify �~ Vue �~ Vite</h2>
-      <nav class="nav">
-        <ul class="list">
-          <li><router-link class="routerLink" to="/">Home</router-link></li>
-          <li><router-link class="routerLink" to="/chat">Chat</router-link></li>
-          <li><router-link class="routerLink" to="/ai">AI</router-link></li>
-          <li><router-link class="routerLink" to="/geo">Geo</router-link></li>
-        </ul>
-      </nav>
-      <button @click="signOut" class="contact">Sign out</button>
-    </header>
-    <section>
-      <router-view/>
-    </section>
-  </main>
+  <authenticator>
+    <!-- 追加 1 -->
+    <template v-slot="{ user, signOut }">
+      <main>
+        <header>
+          <h2>Amplify × Vue × Vite</h2>
+          <nav class="nav">
+            <ul class="list">
+              <li><router-link class="routerLink" to="/">Home</router-link></li>
+              <li><router-link class="routerLink" to="/chat">Chat</router-link></li>
+              <li><router-link class="routerLink" to="/ai">AI</router-link></li>
+              <li><router-link class="routerLink" to="/geo">Geo</router-link></li>
+            </ul>
+          </nav>
+          <!-- 追加 2 -->
+          <button @click="signOut" class="contact">Sign out</button>
+        </header>
+        <section>
+          <!-- 追加 3 -->
+          <router-view :username="user.username" />
+        </section>
+      </main>
+      <!-- 追加 4 -->
+    </template>
+  </authenticator>
 </template>
 
 <style>
