abd977b6fddde286a44cdd2dec607f7241ee5d47
Tetsuhiro Nagasaki <nagasaki.tetsuhiro@xchange.jp>
2023-06-27 17:43:51 +0900

    グループ一覧

--- src/components/IrsChatList.vue
+++ src/components/IrsChatList.vue
@@ -3,15 +3,15 @@
     <div class="wrapper__main">
       <ul class="list-chat">
         <!-- グループ -->
-        <li class="list-chat__item">
+        <li v-for="room in rooms" class="list-chat__item" :key="rooms.id">
           <a href="/irs_chat" class="list-chat__link">
             <div class="list-chat__img"><img src="@/assets/img/img_group_sample.svg" alt="グループ画像"></div>
             <div class="list-chat__text">
               <div class="list-chat__name-time">
-                <p class="list-chat__name">所属グループ名</p>
-                <p class="list-chat__time">18:08</p>
+                <p class="list-chat__name">{{ room.branchName }}</p>
+                <p class="list-chat__time">{{ room.updatedAt }}</p>
               </div>
-              <p class="list-chat__comment">本日のミーティングは開催場所が第１本日のミーティングは開催場所が第１本日テストテストテストテスト</p>
+              <p class="list-chat__comment">test本日のミーティングは開催場所が第１本日のミーティングは開催場所が第１本日テストテストテストテスト</p>
             </div>
           </a>
         </li>
@@ -21,8 +21,27 @@
 </template>
 
 <script>
+import {API, graphqlOperation} from "@aws-amplify/api";
+import {listRooms} from '@/graphql/queries';
+import { ref, onBeforeUnmount, onUpdated } from 'vue';
+
 export default {
-  name: "IrsChatList"
+  name: "IrsChatList",
+  setup () {
+    const rooms = ref([]);
+    const fetch = async () => {
+      try {
+        const response = await API.graphql(graphqlOperation(listRooms, { filter: {} ,limit: 100 }))
+        rooms.value = response.data.listRooms.items.sort((a, b) => (a.branchId > b.branchId ? 1 : -1))
+      } catch (error) {
+        console.dir(error)
+      }
+    }
+    fetch();
+    return {
+      rooms,
+    };
+  }
 }
 </script>
 
