3e8239c920fe9e01e043fc9615fc168a57176f50
Tetsuhiro Nagasaki <nagasaki.tetsuhiro@xchange.jp>
2023-07-04 16:01:11 +0900

    日付形式を修正

--- src/components/IrsChatList.vue
+++ src/components/IrsChatList.vue
@@ -9,9 +9,9 @@
             <div class="list-chat__text">
               <div class="list-chat__name-time">
                 <p class="list-chat__name">{{ room.branchName }}</p>
-                <p class="list-chat__time">{{ room.updatedAt }}</p>
+                <p class="list-chat__time">{{ formatSendAt(room.roomLastMessageUpdatedAt) }}</p>
               </div>
-              <p class="list-chat__comment">test本日のミーティングは開催場所が第１本日のミーティングは開催場所が第１本日テストテストテストテスト</p>
+              <p class="list-chat__comment">{{ room.roomLastMessage }}</p>
             </div>
           </a>
         </li>
@@ -21,18 +21,31 @@
 </template>
 
 <script>
-import {API, graphqlOperation} from "@aws-amplify/api";
-import {listRooms} from '@/graphql/queries';
+import { API, graphqlOperation } from '@aws-amplify/api';
+import { listRooms, getMessage } from '@/graphql/queries';
 import { ref, onBeforeUnmount, onUpdated } from 'vue';
+import mixInCommon from '@/js/mixins/common'
 
 export default {
   name: "IrsChatList",
+  mixins: [mixInCommon],
   setup () {
     const rooms = ref([]);
     const fetch = async () => {
       try {
         const response = await API.graphql(graphqlOperation(listRooms, { filter: {} ,limit: 100 }))
         rooms.value = response.data.listRooms.items.sort((a, b) => (a.branchId > b.branchId ? 1 : -1))
+        for (const element of rooms.value) {
+          let lastMessageUpdatedAt = ''
+          let lastMessage = ''
+          if (!element.roomLastMessageId) {
+            // const getMessageResponse =   API.graphql(graphqlOperation(getMessage, { id: element.roomLastMessageId } ))
+            // lastMessageUpdatedAt = getMessageResponse.data.getMessage.createdAt
+            // lastMessage = getMessageResponse.data.getMessage.content
+          }
+          element.roomLastMessageUpdatedAt = lastMessageUpdatedAt
+          element.roomLastMessage = lastMessage
+        }
       } catch (error) {
         console.dir(error)
       }
@@ -40,15 +53,15 @@ export default {
 
     fetch();
     return {
-      rooms,
+      rooms
     };
   },
   methods: {
     // チャットページへ遷移
-    moveIrsChatPage (roomId, roomName) {
+    moveIrsChatPage(roomId, roomName) {
       this.$store.commit('room/setBranchId', roomId)
       this.$store.commit('room/setBranchName', roomName)
-      this.$router.push({ name: 'Irschat', params: { }})
+      this.$router.push({name: 'Irschat', params: {}})
     }
   }
 }
---
+++ src/js/mixins/common.js
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+
+export default {
+    methods: {
+        formatSendAt (sendAt) {
+            if (sendAt === '' || sendAt === null) {
+                return ' '
+            }
+            const sendAtString = sendAt.replace(/T|Z/g, ' ')
+            const dt = DateTime.fromSQL(sendAtString)
+            const now = DateTime.now()
+            let afterString = ''
+            let afterHours = 0
+            let afterMinute = 0
+            const diffDay = dt.diff(now, 'days')
+            const diffMinute = dt.diff(now, 'minutes')
+            if (diffDay.days !== 0) {
+                return dt.toLocaleString(DateTime.DATETIME_SHORT)
+            }
+
+            afterHours =  diffMinute.minutes / 60
+            afterMinute = diffMinute.minutes % 60
+
+            if (afterHours > 0 ) {
+                afterString = afterString + afterHours + '時間 '
+            }
+            afterString = afterString + afterMinute + '分前'
+
+            return afterString
+        }
+    }
+}
